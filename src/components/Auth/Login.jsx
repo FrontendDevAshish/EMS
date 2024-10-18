a697405128287f5f7b4fa6ea454b10ce146ceb0e
Ashish <kumar.29ashish.95.@gmail.com>
2024-10-18 12:05:28 +0530

    login UI

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="flex h-screen w-screen items-center justify-center">
+        <div className="border-2 border-red-600">
+            <form className="flex flex-col items-center justify-center">
+                <input className="text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-white" type="email" placeholder="Enter your email"/>
+                <input className="text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full mt-3 placeholder:text-white" type="password" placeholder="Enter your password"/>
+                <button>Login</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
