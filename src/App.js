5a9b1ba7c9d8d701e259ba94fa6ca03d60b8b775
{Ashish : Dev } <134873658+FrontendDevAshish@users.noreply.github.com>
2024-11-27 09:06:00 +0000

    UI changes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
   return (
     <div className="App">
    <>
+   <Login/>
     {/* <EmployeeDashboard /> */}
-    <AdminDashboard/>
+    {/* <AdminDashboard/> */}
 
    </>
     </div>
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
             <form onSubmit={ (e)=>{submitHandler(e)} } className="flex flex-col items-center justify-center">
                 <input value={email} onChange={ (e)=>setEmail(e.target.value) } required className=" outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400" type="email" placeholder="Enter your email"/>
                 <input value={password} onChange={ (e)=>setPassword(e.target.value) } required className=" outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full mt-3 placeholder:text-gray-400" type="password" placeholder="Enter your password"/>
-                <button className="mt-5 text-white outline-none  border-none bg-emerald-600 text-xl py-3 px-5 rounded-full  placeholder:text-white">Login</button>
+                <button className="mt-7 text-white outline-none border-none bg-emerald-700 text-xl py-2 px-8 w-full rounded-full placeholder:text-white">Login</button>
             </form>
         </div>
     </div>
